638b0f2765176cb343876df9350964e6e6ced45c
Joonas Pelttari <joonas.pelttari@tuni.fi>
2021-10-05 22:38:53 +0300

    one-hot labels implemented

--- Exercise4/NeuralClassifier.py
+++ Exercise4/NeuralClassifier.py
@@ -2,9 +2,10 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, Dropout, MaxPooling2D, AveragePooling2D
 import keras
+
 
 def neural(trImages, trLabels, testImages, testLabels):
     # reshape vectors of 3072 to 32x32x3 images
@@ -14,42 +15,37 @@ def neural(trImages, trLabels, testImages, testLabels):
     trImages = trImages / 255.0
     testImages = testImages / 255.0
     # convert labels (0-9) to one-hots. e.g. 4 -> [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
-    labelsOneHot = np.zeros((trLabels.size, trLabels.max() + 1), dtype=int)
-    labelsOneHot[np.arange(trLabels.size), trLabels] = 1
+    trOneHot = np.zeros((trLabels.size, trLabels.max() + 1), dtype='float32')
+    trOneHot[np.arange(trLabels.size), trLabels] = 1
+    testOneHot = np.zeros((testLabels.size, testLabels.max() + 1), dtype='float32')
+    testOneHot[np.arange(testLabels.size), testLabels] = 1
 
     # define model and layers
-    #model = keras.Sequential()
-    #model.add(layers.Dense(5, input_shape=(32, 32, 3), activation='relu'))
+    model = Sequential()
+    numEpochs = 60
+    lr = 0.1
+    # model.add(Conv2D(32, (5, 5), input_shape=(32, 32, 3), activation='relu'))
+    # model.add(Conv2D(16, (3, 3), input_shape=(32, 32, 3), activation='relu'))
+    model.add(Flatten(input_shape=(32, 32, 3)))
+    model.add(Dense(128, activation='relu'))
     # output layer
-    #model.add(layers.Dense(10, activation='sigmoid'))
+    model.add(Dense(10, activation='sigmoid'))
 
-    model = tf.keras.Sequential([
-        tf.keras.layers.Flatten(input_shape=(32, 32, 3)),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(10)
-    ])
-    opt = keras.optimizers.SGD(learning_rate=0.01)
+    opt = keras.optimizers.SGD(learning_rate=lr)
     model.compile(optimizer=opt,
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
                   metrics=['accuracy'])
-
     model.summary()
-    hist = model.fit(trImages, trLabels, epochs=60)
+    hist = model.fit(trImages, trOneHot, epochs=numEpochs, verbose=1)
+
     plt.plot(hist.history['loss'])
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.show()
 
-    #opt = keras.optimizers.SGD(learning_rate=0.1)
-    #model.compile(optimizer=opt, loss='mse', metrics=['mse'])
-
-    #training
-    #usedEpochs = 1
-    #model.fit(images, labelsOneHot, epochs=usedEpochs, verbose=1)
-
-    #calculate accuracy with test images
-    loss, acc = model.evaluate(testImages, testLabels, verbose=2)
-
+    # calculate accuracy with test images
+    print("test data accuracy:")
+    loss, acc = model.evaluate(testImages, testOneHot, verbose=2)
 
 
 # returns classification accuracy(%) of provided labels
@@ -99,8 +95,7 @@ testingLabels = unpickle('cifar-10-batches-py/test_batch')["labels"]
 testingLabels = np.asarray(testingLabels)
 
 # Used data sizes
-usedTestImages = 100  # 1-10000
+usedTestImages = 10000  # 1-10000
 usedTrainingImages = 50000
-
 neural(trainingData[0:usedTrainingImages], trainingLabels[0:usedTrainingImages],
        testingData[0:usedTestImages], testingLabels[0:usedTestImages])
