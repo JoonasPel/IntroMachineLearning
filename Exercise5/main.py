fd51333fe5af4557ca33f381033ba796bf46dd26
Joonas Pelttari <joonas.pelttari98@gmail.com>
2021-10-16 00:06:14 +0300

    reinforcement learning implemented

--- Exercise5/main.py
+++ Exercise5/main.py
@@ -1,34 +1,46 @@
 # Load OpenAI Gym and other necessary packages
 import gym
 import random
-import numpy
+import numpy as np
 import time
 
 # Environment
-env = gym.make("Taxi-v3")
+env = gym.make("Taxi-v3").env
 
 # Training parameters for Q learning
-alpha = 0.9 # Learning rate
-gamma = 0.9 # Future reward discount factor
-num_of_episodes = 1000
-num_of_steps = 500 # per each episode
+alpha = 1.0  # Learning rate .WAS 0.9
+gamma = 0.9  # Future reward discount factor
+num_of_episodes = 500
+num_of_steps = 500  # per each episode
 
 # Q tables for rewards
-#Q_reward = -100000*numpy.ones((500,6)) # All same
-Q_reward = -100000*numpy.random.random((500, 6)) # Random
+# Q_reward = -100000*numpy.ones((500,6)) # All same
+Q_reward = 1 * np.random.random((500, 6))  # Random
 
 # Training w/ random sampling of actions
-# YOU WRITE YOUR CODE HERE
+print("Training...")
+for episode in range(0, num_of_episodes):
+    old_state = env.reset()
+    for step in range(0, num_of_steps):
+        random_action = random.randrange(0, 6)
+        new_state, reward, done, info = env.step(random_action)
+        Q_reward[old_state][random_action] = reward + gamma * np.amax(Q_reward[new_state])
+        old_state = new_state
+        if done:
+            break
 
 # Testing
+print("Testing starts")
+time.sleep(1)
+
 state = env.reset()
 tot_reward = 0
 for t in range(50):
-    action = numpy.argmax(Q_reward[state,:])
+    action = np.argmax(Q_reward[state, :])
     state, reward, done, info = env.step(action)
     tot_reward += reward
     env.render()
     time.sleep(1)
     if done:
-        print("Total reward %d" %tot_reward)
+        print("Total reward %d" % tot_reward)
         break
